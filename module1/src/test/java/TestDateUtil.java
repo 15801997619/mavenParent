30c8d853e353deae887b79764275264eb8544626
15801997619 <1562412509@qq.com>
2019-05-06 17:22:01 +0800

    使用Idea创建普通的maven多module（父、子）项目创建demo

---
+++ module1/src/test/java/TestDateUtil.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName:TestDateUtil
+ * @Description: TODO
+ * @Author: xu.liu
+ * @Date:2019/5/6 14:25
+ * @Version:V1.0
+ **/
+public class TestDateUtil {
+
+    public static void main(String[] args) {
+        System.out.println("当前时间：" + DateUtil.getCurrentTime24SSS());
+    }
+}
